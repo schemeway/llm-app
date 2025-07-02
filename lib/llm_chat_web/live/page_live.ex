211ef5d4200896cc8c8e12704386e98d4d38d2ef
Dominique Boucher <schemeway@gmail.com>
2025-07-02 13:55:29 -0400

    adds tooltips for the tool description

--- lib/llm_chat_web/live/page_live.ex
+++ lib/llm_chat_web/live/page_live.ex
@@ -74,7 +74,7 @@ defmodule LlmChatWeb.PageLive do
                   phx-value-tool={tool.name()}
                   class="form-checkbox h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                 />
-                <span class="ml-2 text-gray-700"><%= tool.name() %></span>
+                <span title={tool.description()} class="ml-2 text-gray-700"><%= tool.name() %></span>
               </label>
             </div>
           <% end %>
