aa29f7d570d99147a74ac8ca3873d0a00a6b776e
Dominique Boucher <schemeway@gmail.com>
2025-09-19 14:53:34 -0400

    small refactoring

--- lib/llm/history.ex
+++ lib/llm/history.ex
@@ -3,8 +3,9 @@ defmodule Llm.History do
   def save_conversation(history, id, messages) do
     dir = get_history_path()
     File.mkdir_p!(dir)
-    file_path = Path.join(dir, "#{id}")
-    File.write!(file_path, Jason.encode!(messages, pretty: true))
+    Path.join(dir, "#{id}")
+    |> File.write!(Jason.encode!(messages, pretty: true))
+
     Map.put(history, id, messages)
   end
 
@@ -12,11 +13,13 @@ defmodule Llm.History do
     dir = get_history_path()
 
     File.ls!(dir)
-    |> Enum.map(fn file ->
-      {:ok, content} = File.read(Path.join(dir, file))
-      {file, Jason.decode!(content)}
-    end)
+    |> Enum.map(&read_file(dir, &1))
     |> Enum.into(Map.new())
+  end
+
+  defp read_file(dir, file_name) do
+      {:ok, content} = File.read(Path.join(dir, file_name))
+      {file_name, Jason.decode!(content)}
   end
 
   defp get_history_path() do
