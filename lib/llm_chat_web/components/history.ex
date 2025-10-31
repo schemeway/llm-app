90fee5294cd72fd7d5452fe6eae1f2d7fff22e5c
Dominique Boucher <schemeway@gmail.com>
2025-10-31 08:17:29 -0400

    highlights the current conversation

--- lib/llm_chat_web/components/history.ex
+++ lib/llm_chat_web/components/history.ex
@@ -5,23 +5,30 @@
   def history(assigns) do
     ~H"""
       <div class="flex flex-col w-1/5 h-screen bg-gray-200">
-        <div class="flex-grow p-4 overflow-y-auto space-y-4">
+        <div class="h-1/2 flex-grow p-4 space-y-4 overflow-y-auto">
           <h1 class="text-xl font-bold text-gray-800 mb-4">Conversations</h1>
           <div class="history">
             <%= for {id, description} <- @conversations do %>
               <div class="history-item relative group">
+              <%= if @id == id do %>
+                <ul class="cursor-pointer bg-blue-50 rounded-xl hover:bg-gray-50 hover:rounded-xl border-b border-gray-300 py-2 truncate text-ellipsis" phx-click="load_conversation" phx-value-id={id}>
+                  <span class="font-semibold hover:text-blue-900 "><%= description %></span>
+                  <br/> <span class="text-xs text-gray-500"><%= id %></span>
+                </ul>
+              <% else %>
                 <ul class="cursor-pointer hover:bg-gray-50 hover:rounded-xl border-b border-gray-300 py-2 truncate text-ellipsis" phx-click="load_conversation" phx-value-id={id}>
                   <span class="font-semibold hover:text-blue-900 "><%= description %></span>
                   <br/> <span class="text-xs text-gray-500"><%= id %></span>
                 </ul>
+              <% end %>
                 <button
                   class="absolute right-2 top-2 opacity-0 group-hover:opacity-100 transition-opacity bg-gray-500 text-white rounded-full w-6 h-6 flex items-center justify-center text-xs hover:bg-gray-600"
                   phx-click="delete_conversation"
                   phx-value-id={id}
                 >x</button>
-              </div>
-            <% end %>
-          </div>
+               </div>
+             <% end %>
+           </div>
         </div>
       </div>
     """
